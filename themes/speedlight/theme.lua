c449b22446f2f37c5d0da9deeda8ebaae46545fb
speedlight <tao.ceel@gmail.com>
2017-03-10 16:07:30 -0500

    awful.util. fixes, add xfce4-screenshooter key bind

--- themes/speedlight/theme.lua
+++ themes/speedlight/theme.lua
@@ -93,7 +93,7 @@ local separators = lain.util.separators
 
 -- Textclock
 local clockicon = wibox.widget.imagebox(theme.widget_clock)
-local clock = lain.widgets.abase({
+local clock = lain.widget.watch({
     timeout  = 60,
     cmd      = " date +'%a %d %b %R'",
     settings = function()
@@ -102,7 +102,7 @@ local clock = lain.widgets.abase({
 })
 
 -- Calendar
-theme.cal = lain.widgets.calendar({
+theme.cal = lain.widget.calendar({
     attach_to = { clock.widget },
     notification_preset = {
         font = "xos4 Terminus 10",
@@ -134,8 +134,8 @@ local mail = lain.widgets.imap({
 
 -- MPD
 local mpdicon = wibox.widget.imagebox(theme.widget_music)
-mpdicon:buttons(awful.util.table.join(awful.button({ }, 1, function () awful.util.spawn_with_shell(musicplr) end)))
-theme.mpd = lain.widgets.mpd({
+mpdicon:buttons(awful.util.table.join(awful.button({ }, 1, function () awful.spawn_with_shell(musicplr) end)))
+theme.mpd = lain.widget.mpd({
     settings = function()
         if mpd_now.state == "play" then
             artist = " " .. mpd_now.artist .. " "
@@ -156,7 +156,7 @@ theme.mpd = lain.widgets.mpd({
 
 -- MEM
 local memicon = wibox.widget.imagebox(theme.widget_mem)
-local mem = lain.widgets.mem({
+local mem = lain.widget.mem({
     settings = function()
         widget:set_markup(markup.font(theme.font, " " .. mem_now.used .. "MB "))
     end
@@ -164,7 +164,7 @@ local mem = lain.widgets.mem({
 
 -- CPU
 local cpuicon = wibox.widget.imagebox(theme.widget_cpu)
-local cpu = lain.widgets.cpu({
+local cpu = lain.widget.cpu({
     settings = function()
         widget:set_markup(markup.font(theme.font, " " .. cpu_now.usage .. "% "))
     end
@@ -172,7 +172,7 @@ local cpu = lain.widgets.cpu({
 
 -- Coretemp
 local tempicon = wibox.widget.imagebox(theme.widget_temp)
-local temp = lain.widgets.temp({
+local temp = lain.widget.temp({
     settings = function()
         widget:set_markup(markup.font(theme.font, " " .. coretemp_now .. "°C "))
     end
@@ -180,7 +180,7 @@ local temp = lain.widgets.temp({
 
 -- / fs
 local fsicon = wibox.widget.imagebox(theme.widget_hdd)
-theme.fs = lain.widgets.fs({
+theme.fs = lain.widget.fs({
     options  = "--exclude-type=tmpfs",
     notification_preset = { fg = theme.fg_normal, bg = theme.bg_normal, font = "xos4 Terminus 10" },
     settings = function()
@@ -190,7 +190,7 @@ theme.fs = lain.widgets.fs({
 
 -- Battery
 local baticon = wibox.widget.imagebox(theme.widget_battery)
-local bat = lain.widgets.bat({
+local bat = lain.widget.bat({
     settings = function()
         if bat_now.status ~= "N/A" then
             if bat_now.ac_status == 1 then
@@ -214,7 +214,7 @@ local bat = lain.widgets.bat({
 
 -- ALSA volume
 local volicon = wibox.widget.imagebox(theme.widget_vol)
-theme.volume = lain.widgets.alsa({
+theme.volume = lain.widget.alsa({
     settings = function()
         if volume_now.status == "off" then
             volicon:set_image(theme.widget_vol_mute)
@@ -232,7 +232,7 @@ theme.volume = lain.widgets.alsa({
 
 -- Net
 local neticon = wibox.widget.imagebox(theme.widget_net)
-local net = lain.widgets.net({
+local net = lain.widget.net({
     settings = function()
         widget:set_markup(markup.font(theme.font,
                           markup("#7AC82E", " " .. net_now.received)
@@ -251,7 +251,7 @@ function theme.at_screen_connect(s)
     s.quake = lain.util.quake({ app = awful.util.terminal })
 
     -- If wallpaper is a function, call it with the screen
-    if type(wallpaper) == "function" then
+    if type(theme.wallpaper) == "function" then
         theme.wallpaper = theme.wallpaper(s)
     end
     gears.wallpaper.maximized(theme.wallpaper, s, true)
@@ -283,7 +283,7 @@ function theme.at_screen_connect(s)
         layout = wibox.layout.align.horizontal,
         { -- Left widgets
             layout = wibox.layout.fixed.horizontal,
-            spr,
+            --spr,
             s.mytaglist,
             s.mypromptbox,
             spr,
