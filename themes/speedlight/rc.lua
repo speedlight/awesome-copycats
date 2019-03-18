a04fd0551c602bc720d21da1f44884228e86b6d7
Carlos Speedlight <tao.ceel@gmail.com>
2019-03-17 21:03:19 -0500

    updating from upstream

--- themes/speedlight/rc.lua
+++ themes/speedlight/rc.lua
@@ -56,7 +56,7 @@ local function run_once(cmd_arr)
     end
 end
 
-run_once({ "unclutter -root", "xscreensaver -no-splash", "nm-applet" }) -- entries must be separated by commas
+run_once({ "xscreensaver -no-splash", "nm-applet" }) -- entries must be separated by commas
 
 -- This function implements the XDG autostart specification
 --[[
@@ -263,14 +263,12 @@ globalkeys = my_table.join(
         function ()
             awful.client.focus.byidx( 1)
         end,
-        {description = "focus next by index", group = "client"}
-    ),
+        {description = "focus next by index", group = "client"}),
     awful.key({ altkey,           }, "k",
         function ()
             awful.client.focus.byidx(-1)
         end,
-        {description = "focus previous by index", group = "client"}
-    ),
+        {description = "focus previous by index", group = "client"}),
 
     -- By direction client focus
     awful.key({ modkey }, "j",
@@ -385,10 +383,6 @@ globalkeys = my_table.join(
               end,
               {description = "restore minimized", group = "client"}),
 
-    -- Dropdown application
-    --awful.key({ modkey, }, "z", function () awful.screen.focused().quake:toggle() end,
-    --          {description = "dropdown application", group = "launcher"}),
-
     --[[ Widgets popups
     awful.key({ altkey, }, "c", function () lain.widget.calendar.show(7) end,
               {description = "show calendar", group = "widgets"}),
@@ -499,34 +493,7 @@ globalkeys = my_table.join(
     awful.key({ modkey }, "h", function () awful.spawn(htop) end,
         {description = "htop", group = "hotkeys"}),
     awful.key({ modkey }, "l", function () awful.spawn("systemctl suspend", false) end,
-        {description = "suspend", group = "hotkeys"}),
-    -- Default
-    --[[ Menubar
-    awful.key({ modkey }, "p", function() menubar.show() end,
-              {description = "show the menubar", group = "launcher"})
-    --]]
-    --[[ dmenu
-    awful.key({ modkey }, "x", function ()
-            os.execute(string.format("dmenu_run -i -fn 'Monospace' -nb '%s' -nf '%s' -sb '%s' -sf '%s'",
-            beautiful.bg_normal, beautiful.fg_normal, beautiful.bg_focus, beautiful.fg_focus))
-        end,
-        {description = "show dmenu", group = "launcher"})
-    --]]
-    -- Prompt
-    awful.key({ modkey }, "r", function () awful.screen.focused().mypromptbox:run() end,
-              {description = "run prompt", group = "launcher"}),
-
-    awful.key({ modkey }, "x",
-              function ()
-                  awful.prompt.run {
-                    prompt       = "Run Lua code: ",
-                    textbox      = awful.screen.focused().mypromptbox.widget,
-                    exe_callback = awful.util.eval,
-                    history_path = awful.util.get_cache_dir() .. "/history_eval"
-                  }
-              end,
-              {description = "lua execute prompt", group = "awesome"})
-    --]]
+        {description = "suspend", group = "hotkeys"})
 )
 
 clientkeys = my_table.join(
@@ -546,20 +513,16 @@ clientkeys = my_table.join(
               {description = "move to master", group = "client"}),
     awful.key({ modkey,           }, "o",      function (c) c:move_to_screen()               end,
               {description = "move to screen", group = "client"}),
-    --awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
-    --          {description = "toggle keep on top", group = "client"}),
     awful.key({ modkey,           }, "n",
         function (c)
-            -- The client currently has the input focus, so it cannot be
-            -- minimized, since minimized clients can't have the focus.
             c.minimized = true
-        end ,
+        end,
         {description = "minimize", group = "client"}),
     awful.key({ modkey,           }, "m",
         function (c)
             c.maximized = not c.maximized
             c:raise()
-        end ,
+        end,
         {description = "maximize", group = "client"})
 )
 
